3d62bc1416338015eb589fce1a05382710f74155
ut <ut@MacBookAir.local>
2016-01-28 23:18:45 +0900

    Add remove_break action

--- script/action/paste_as_html.js
+++ script/action/paste_as_html.js
@@ -1,3 +1,9 @@
+/*
+ * paste_as_html.js
+ * 
+ * 改行をbrタグに変換し、pタグでwrapして返す
+ */
+
 var text = clipText.replace(/[\n\r]/g, '<br />\n');
 text = '<p>\n' + text + '\n</p>';
 
---
+++ script/action/remove_break.js
@@ -0,0 +1,9 @@
+/*
+ * remove_break.js
+ * 
+ * 改行を削除して返す
+ */
+
+var text = clipText.replace(/[\n\r]/g, '');
+
+return text;
